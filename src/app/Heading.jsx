3b3858a4672be9278c4071e30a663813cea1bf8d
juliaszbrt <112975933+juliaszbrt@users.noreply.github.com>
2023-09-23 20:27:16 -0400

    add goose :3

--- src/app/Heading.jsx
+++ src/app/Heading.jsx
@@ -2,10 +2,10 @@ import Image from "next/image"
 
 const Heading = () => {
   return (
-    <div className="flex justify-center mt-[0.7rem] sm:mt-[3.5rem]">
-        <h1 className="tracking-[0.6rem] sm:tracking-[1rem] text-[3rem] text-[#F3683B] text-center sm:text-[7rem]">Gøøse</h1>
-        <div>
-            <Image></Image>
+    <div className="flex justify-center items-center mt-[0.7rem] sm:mt-[3.5rem]">
+        <h1 className="font-bold tracking-[0.6rem] sm:tracking-[1rem] text-[3rem] text-[#F3683B] text-center sm:text-[7rem]">Gøøse</h1>
+        <div className="relative h-[3.7rem] w-[3.7rem] sm:h-[8rem] sm:w-[8rem]">
+            <Image fill src="/goose.png" className="object-cover"></Image>
         </div>
     </div>
   )
